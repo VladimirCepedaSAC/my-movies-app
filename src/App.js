83f906d0608be9d3f5a8f714615720c46aa80dde
Vladimir <Vladimir@VLADIMIR-LAPTOP>
2024-11-21 08:41:46 -0400

    Cambio de nombre de la app

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Peliculas sugeridas</h1>
+          <h1>RandomFlix</h1>
           <div class="movie-container" >
           {movies.map((movie)=>(
             <MovieCard 
